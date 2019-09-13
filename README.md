f505b5df26a990ad6a790b4ce450a19435434363
Tommy Hofmann <thofma@gmail.com>
2019-09-13 13:11:37 +0200

    Add autogenerated files to build libntl

---
+++ README.md
@@ -0,0 +1,9 @@
+# NTLBuilder2
+
+[![Build Status](https://travis-ci.org/thofma/NTLBuilder2.svg?branch=master)](https://travis-ci.org/thofma/NTLBuilder2)
+
+This repository builds binary artifacts for the libntl project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/thofma/NTLBuilder2/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,76 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libntl"
+version = v"10.5.0"
+
+# Collection of sources required to build libntl
+sources = [
+    "https://www.shoup.net/ntl/ntl-10.5.0.tar.gz" =>
+    "b90b36c9dd8954c9bc54410b1d57c00be956ae1db5a062945822bbd7a86ab4d2",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+wget https://www.mathematik.uni-kl.de/~thofmann/configure.patch
+cd ntl-10.5.0/
+patch -p1 < $WORKSPACE/srcdir/configure.patch 
+cd $WORKSPACE/srcdir
+CC_OLD=$CC
+CC=$CC_FOR_BUILD
+CXX_OLD=$CXX
+CXX=$CXX_FOR_BUILD
+LIBTOOL_OLD=$LIBTOOL
+LIBTOOL=/usr/bin/libtool
+LD_OLD=$LD
+LD=$LD_FOR_BUILD
+LDFLAGS_OLD=$LDFLAGS
+LDFLAGS=""
+wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
+tar -xvf gmp-6.1.2.tar.bz2
+cd gmp-6.1.2
+make clean
+CC=$CC_FOR_BUILD CXX=$CXX_FOR_BUILD ./configure --enable-cxx --enable-shared
+LD=$LD_FOR_BUILD LDFLAGS="" CC=$CC_FOR_BUILD CXX=$CXX_FOR_BUILD make -j${nproc}
+make install
+CC=$CC_OLD
+CXX=$CXX_OLD
+LIBTOOL=$LIBTOOL_OLD
+LD=$LD_OLD
+LDFLAGS=$LDFLAGS_OLD
+cd $WORKSPACE/srcdir
+export CPP_FLAGS=-I$prefix/include
+export LD_LIBRARY_PATH=$target/lib:$LD_LIBRARY_PATH
+export LDFLAGS=-Wl,-rpath,$prefix/lib
+cd $WORKSPACE/srcdir
+cd ntl-10.5.0/
+cd src
+./configure PREFIX=$prefix DEF_PREFIX=$prefix SHARED=on NTL_THREADS=off NTL_EXCEPTIONS=off NTL_GMP_LIP=on CXXFLAGS=-I$prefix/include TUNE=x86 CXX_FOR_BUILD="$CXX_FOR_BUILD" LD_FOR_BUILD="$LD_FOR_BUILD" CXX="$CXX" HOST=$target
+make -j${nproc}
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libntl", Symbol(""))
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/JuliaMath/GMPBuilder/releases/download/v6.1.2-2/build_GMP.v6.1.2.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
